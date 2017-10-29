e63a7d4caceccaa7b648f6ee3d4d049da1526a9f
DaokeLegend <lijunyuwork@163.com>
2017-10-29 20:31:27 +0800

    CoreML

---
+++ JJCoreML_demo1/JJCoreML_demo1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JJCoreML_demo1
+//
+//  Created by lucy on 2017/10/29.
+//  Copyright © 2017年 com.daoKeLegend. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JJCoreML_demo1/JJCoreML_demo1/ViewController.h
@@ -0,0 +1,19 @@
+//
+//  ViewController.h
+//  JJCoreML_demo1
+//
+//  Created by lucy on 2017/10/29.
+//  Copyright © 2017年 com.daoKeLegend. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+/**
+     这里是CoreML的开始
+ */
+
+@interface ViewController : UIViewController
+
+
+@end
+
